3c91ed5d8710140e3bc50bc4394c5cf373c34eb3
Ubuntu <ubuntu@ip-172-31-42-134.us-east-2.compute.internal>
2026-07-06 13:41:58 +0000

    Deploy frontend application to AWS EC2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "http://localhost:5173", "http://127.0.0.1:5173", "http://13.58.158.65:5173"
 ]
 
 ROOT_URLCONF = 'backend.urls'
